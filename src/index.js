a1bd345db7a15c16018a79cad95948cd17e17380
Eremor <artem.s13@gmail.com>
2021-02-15 11:21:09 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array == 0 || array == '' || array == undefined) ? 0 : array.sort((x, y) => x - y)[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array == 0 || array == '' || array == undefined) ? 0 : array.sort((x, y) => y - x)[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (array == 0 || array == '' || array == undefined) ? 0 : array.reduce((x, y) => x + y)/array.length
 }
